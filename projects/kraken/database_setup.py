d6d31385bef448340dd13577ddb208864c234f84
diverdano <diverdano@gmail.com>
2018-07-05 22:49:18 -0400

    creating controller and model

---
+++ projects/kraken/database_setup.py
@@ -0,0 +1,80 @@
+#import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class Deposits(Base):
+    __tablename__       = 'account'
+    id                  = Column(String(40), nullable = False)
+
+    account             =
+    address             =
+    amount              =
+    bip125-replaceable  =
+    blockhash           =
+    blockindex          =
+    blocktime           =
+    category            =
+    confirmations       =
+    involvesWatchonly   =
+    label               =
+    time                =
+    timereceived        =
+    txid                =
+    vout                =
+    walletconflicts     =
+
+    ''' reference data, couple be moved to DB if needed '''
+    secType             = ['equity','option','future','bond']
+    exchange            = ['NASDAQ','CME','CBOE','NYSE','OTC']
+
+
+
+class Acount(Base):
+    pass
+def Accounts(trans):
+    # * Wesley Crusher: mvd6qFeVkqH6MNAS2Y2cLifbdaX5XUkbZJ
+    # * Leonard McCoy: mmFFG4jqAtw9MoCC88hw5FNfreQWuEHADp
+    # * Jonathan Archer: mzzg8fvHXydKs8j9D2a8t7KpSXpGgAnk4n
+    # * Jadzia Dax: 2N1SP7r92ZZJvYKG2oNtzPwYnzw62up7mTo
+    # * Montgomery Scott: mutrAf4usv3HKNdpLwVD4ow2oLArL6Rez8
+    # * James T. Kirk: miTHhiX3iFhVnAEecLjybxvV5g8mKYTtnM
+    # * Spock: mvcyJMiAcSXKAEsQxbW9TYZ369rsMG6rVV
+    pass
+
+class Asset(Base):
+    __tablename__   = 'asset'
+    id              = Column(Integer, primary_key = True, autoincrement = True)
+    type            = Column(String(10), nullable = False)  # equity, option, future, bond
+    exchange        = Column(String(10), nullable = False)  # NASDAQ, CME, CBOE, NYSE
+    symbol          = Column(String(20), nullable = False)  # security symbol or bond cusip
+    description     = Column(String(20), nullable = False)
+    size            = Column(Integer, nullable = False)     # contract size; stock = $1, S&P 500 e-mini = $50, vix = $1000
+    coupon          = Column(Float)                         # only for bonds
+    expiry          = Column(String(25))                    # for equity options, futures, 'maturity' for bonds
+    strike          = Column(Integer)                       # for options
+    def __repr__(self):
+        return("\n\tasset id: {0}\t{1}\t{2}\t{3}\t{4}\t{5}".format(self.id, self.type, self.exchange, self.symbol, self.description, self.size))
+
+class Allocation(Base):
+    __tablename__   = 'allocation'
+    id              = Column(Integer, primary_key = True, autoincrement = True)
+    date_mod        = Column(String(25), nullable = False)
+    portfolio       = Column(String(10), nullable = False)          # use a string name for portfolio
+    asset_id        = Column(Integer, ForeignKey('asset.id'))
+    asset           = relationship(Asset)
+    allocation      = Column(Float, nullable = False)
+    def __repr__(self):
+#        return("\n\tallocation id: {0}\t{1}\t{2}\t{3}\t{4:.2%}".format(self.id, self.portfolio, self.asset_id, self.asset, self.allocation))
+        return("\n\tallocation id: {0}\t{1}\t{2}\t{3}\t{4:.2%}".format(self.id, self.portfolio, self.asset_id, self.asset, self.allocation))
+
+## insert at end of file ##
+def createDBsession(db_info):
+    ''' create session to db '''
+    return sessionmaker()(bind=create_engine(db_info))
+#engine = create_engine('sqlite:///simco.db')
+#Base.metadata.create_all(engine)        # suppress this if creating DB via SQL script?
--- projects/kraken/validate_deposit.py
+++ projects/kraken/validate_deposit.py
@@ -1,12 +1,13 @@
 #!/usr/env python
 import pandas as pd
 import numpy as np
-import psycopg2
+# import psycopg2
 import argparse
 import logging
 import re
-from sqlalchemy import create_engine
+# from sqlalchemy import create_engine
 import io
+import simplejson as json
 
 # -- logging --
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -22,6 +23,11 @@ args = vars(ap.parse_args())
 logger.info('\n\tlogging is on\n')
 
 # -- helper functions --
+
+def readJSONFile(file):
+    '''simple read file'''
+    with open(file, 'r') as infile: data=infile.read()
+    return(json.loads(data))
 
 def getMaxLengths(df):
     '''return max length for each column in df'''
@@ -41,21 +47,7 @@ def setDF(w=None, c=None, r=None, f='{:,.1f}'):
 
 # -- load data --
 
-def getFlights(file = 'flights.csv', sep = '|'):
-    '''return pd dataframe with parsed file data'''
-    logger.info('processing flights')
-    # true_values : list, default None - Values to consider as True
-    # false_values : list, default None
-    # na_filter : boolean, default True - setting to False can improve performance
-    # dtype : Type name or dict of column -> type, default None
-        # Data type for data or columns. E.g. {'a': np.float64, 'b': np.int32}
-        # Use `str` or `object` to preserve and not interpret dtype.
-        # If converters are specified, they will be applied INSTEAD
-        # of dtype conversion.
-    flights = pd.read_csv(filepath_or_buffer = file, sep = sep, dtype='object')
-    flights = flights.rename(columns = {'ORIGAIRPORTNAME':'ORIGINAIRPORTNAME'})     # column name inconsistency in source
-    logger.info('flights found: {:,}'.format(len(flights)))
-    return(flights)
+# pd1.groupby(['address','txid','confirmations','blocktime','category'])['txid'].count()
 
 # -- transform data --
 
@@ -76,126 +68,6 @@ def getAirlines(df, type='df'):
         airlines = airlines_df[['id_airline','name']].to_records(index=False)
         return(airlines)
 
-def getAirports(df, type='df'):
-    '''return airports dataset
-    time zone ids with DST offsets source http://download.geonames.org/export/dump/timeZones.txt
-
-    '''
-    #dft1 = dft1.astype({'a': np.bool, 'c': np.float64})
-    logger.info('processing airports')
-    airports_df_orig = df.groupby('ORIGINAIRPORTCODE', as_index=False)['ORIGINAIRPORTNAME', 'ORIGINCITYNAME', 'ORIGINSTATE', 'ORIGINSTATENAME'].last()
-    airports_df_dest = df.groupby('DESTAIRPORTCODE', as_index=False)['DESTAIRPORTNAME', 'DESTCITYNAME', 'DESTSTATE', 'DESTSTATENAME'].last()
-    airports_df_orig.columns = (['id_airport','name','city','st','state'])      # could join these into 1 statement
-    airports_df_dest.columns = (['id_airport','name','city','st','state'])
-    airports_df = pd.concat([airports_df_orig, airports_df_dest])
-    airports_df['name'] = airports_df['name'].apply(lambda x: x.split(': ')[1])  # strip ':' and trailing text
-    airports_df.sort_values(by=['id_airport'], axis=0, inplace=True)
-    airports_df.drop_duplicates(inplace=True)                                   # perhaps force columns to exclude index...
-    airports_df.reset_index(drop=True, inplace=True)
-    logger.info('airports found: {:,}'.format(len(airports_df)))
-    if airports_df.isnull().values.any():
-        nan_airports_df = airports_df[pd.isnull(airports_df).any(axis=1)]
-        nan_airports    = list(airports_df[pd.isnull(airports_df).any(axis=1)]['id_airport'])     # set aside bad airport names
-        logger.error(['airports with NaN values:', nan_airports_df])
-        OK_airports     = ['LAW', 'OKC','TUL']
-        KS_airports     = ['FOE', 'GCK', 'HYS', 'ICT', 'MHK']
-        airports_df['st'][airports_df.id_airport.isin(KS_airports)]       = "KS"
-        airports_df['state'][airports_df.id_airport.isin(KS_airports)]    = "Kansas"
-        airports_df['st'][airports_df.id_airport.isin(OK_airports)]       = "OK"
-        airports_df['state'][airports_df.id_airport.isin(OK_airports)]    = "Oklahoma"
-        nan_airports_df = airports_df[pd.isnull(airports_df).any(axis=1)]                   # retest for Nan
-        logger.error(['airports with NaN values:', nan_airports_df])
-
-    if type == 'df' : return airports_df
-    else            :
-        airports = airports_df.to_records(index=False)
-        return(airlines)
-
-# 128  FOE         Forbes Field                          Topeka               NaN  NaN --> KS
-# 133  GCK         Garden City Regional                  Garden City          NaN  NaN --> KS
-# 163  HYS         Hays Regional                         Hays                 NaN  NaN --> KS
-# 167  ICT         Wichita Dwight D Eisenhower National  Wichita              NaN  NaN --> KS
-# 195  LAW         Lawton-Fort Sill Regional             Lawton/Fort Sill     NaN  NaN --> OK
-# 227  MHK         Manhattan Regional                    Manhattan/Ft. Riley  NaN  NaN --> KS
-# 254  OKC         Will Rogers World                     Oklahoma City        NaN  NaN --> OK
-# 345  TUL         Tulsa International                   Tulsa                NaN  NaN --> OK
-
-def getAirplanes(df, type='df', replace=True):                         # probably quicker in db
-    '''return airplane dataset'''
-    logger.info('processing airplanes')
-#    airplanes_df = df.groupby(['TAILNUM', 'AIRLINECODE'], as_index=False)['AIRLINECODE'].last()      # removing records where plane flew under different airline
-    airplanes_df = df.groupby('TAILNUM', as_index=False)['AIRLINECODE'].last()      # removing records where plane flew under different airline
-    airplanes_df.columns = (['id_tailnum','id_airline'])
-    logger.info('airplanes records found: {:,}'.format(len(airplanes_df)))
-    logger.info('column names: {}'.format(airplanes_df))
-    # FAA website: http://registry.faa.gov/aircraftinquiry/NNum_Inquiry.aspx
-    if replace:
-        # possible bad tail numbers: 'NKNO', 'UNKNOW', all beginning with '-', 'N998R@', 'NEIDLA'
-        char_list = ["@", "'", "-", "UNKNOW", "NKNO"]                                    # list of characters to replace
-        airplanes_df = airplanes_df['id_tailnum'].apply(lambda x: re.sub("|".join(char_list), "", x).upper()) # should store original and 'fixed'
-    #    airplanes_df['id_tailnum'] = airplanes_df['id_tailnum'].apply(lambda x: x.strip('-'))
-#        airplanes_df = airplanes_df.groupby('id_tailnum', as_index=False)['id_airline'].last()
-#        logger.info('unique airplanes found: {:,}'.format(len(airplanes_df)))
-    if type == 'df' : return airplanes_df
-    else            :
-        airplanes = airplanes_df.to_records(index=False)
-        return(airplanes)
-
-def trimFlights(df, type='df'):
-    '''return flight fact dataset'''
-    logger.info('processing flights - for fact table')
-    drop_columns = [
-        'AIRLINENAME',
-        'ORIGINAIRPORTNAME',          # fixed in getFlights()
-        'ORIGINCITYNAME',
-        'ORIGINSTATE',
-        'ORIGINSTATENAME',
-        'DESTAIRPORTNAME',
-        'DESTCITYNAME',
-        'DESTSTATE',
-        'DESTSTATENAME']
-    new_col_names = [
-        'id_trans',
-        'date_flight',
-        'id_airline',
-        'id_tailnum',
-        'id_flightnum',
-        'id_airport_orig',
-        'id_airport_dest',
-        'time_depart_crs',
-        'time_depart',
-        'time_depart_delay',
-        'time_taxi_out',
-        'time_wheelsoff',
-        'time_wheelson',
-        'time_taxi_in',
-        'time_arrive_crs',
-        'time_arrive',
-        'time_arrive_delay',
-        'time_elapsed_crs',
-        'time_elapsed_act',
-        'stat_cancelled',
-        'stat_diverted',
-        'stat_miles']               # strip miles and keep imperial measure vs metric
-    flight_log                      = df.drop(drop_columns, axis=1, errors='ignore')        # don't need inplace with assignment
-    flight_log.columns              = (new_col_names)
-#    df[['col.name1', 'col.name2'...]] = df[['col.name1', 'col.name2'..]].astype('data_type')
-#   to_datetime
-#   to_timedelta
-    tf_dict                         = {'T':1, 'True':1, 'F':0, 'False':0, 'NA':0, '1':1, '0':0}     # change all T, True to 1 and F, False to 0
-    flight_log['stat_diverted']     = flight_log['stat_diverted'].map(tf_dict)
-    flight_log['stat_cancelled']    = flight_log['stat_cancelled'].map(tf_dict)#.astype('int64', errors='ignore')  # convert upon upload?
-    flight_log['stat_miles']        = flight_log['stat_miles'].apply(lambda x: int(x.split()[0]))
-#     cols_time                       = ['time_depart_crs','time_depart','time_arrive_crs','time_arrive','time_wheelsoff','time_wheelson']
-#     cols_timedelta                  = ['time_elapsed_crs','time_elapsed_act']
-#     cols_int                        = ['id_trans','id_flightnum','time_depart_delay','time_arrive_delay','time_taxi_out','time_taxi_in','time_arrive_crs','time_arrive']
-#     flight_log['date_flight']       = flight_log.date_flight.apply(lambda x: pd.to_datetime(x, format='%Y%M%d').date())
-# #    flight_log[cols_time]           = flight_log[cols_time].apply(lambda x: pd.to_datetime(x, format='%h%m').time()) # fails on nulls
-# #    flight_log[cols_time]           = flight_log[cols_time].apply(lambda x: pd.to_datetime(x, unit='m'))
-#     flight_log[cols_time]           = flight_log[cols_time].apply(lambda x: pd.to_timedelta(x, unit='m'))
-#     flight_log[cols_timedelta]      = flight_log[cols_timedelta].apply(lambda x: pd.to_timedelta(x, unit='m', errors='ignore'))
-#     flight_log[cols_int]            = flight_log[cols_int].apply(lambda x: pd.to_numeric(x))
-    return(flight_log)
 
 # -- objects --
 
@@ -210,8 +82,6 @@ def sqlalchemy2sql(df, table):
     con_str     = 'postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}'.format(user, password, host, port, database)
     logger.info('connecting to IW db')
     engine = create_engine(con_str)
-    # df.to_sql('table_name', engine)
-    # engine = create_engine('postgresql+psycopg2://username:password@host:port/database')
     conn = engine.raw_connection()
     cur = conn.cursor()
     output = io.StringIO()
@@ -251,142 +121,12 @@ class IWDB(object):
         '''copy from data object to db table'''
         self.cur.copy_from(data, table)
     def getDBver(self): self.select(sql='select version()')
-    # def getDBversion(self):
-    #     '''create cursor, get db version, log and close cursor'''
-    #     conn = None
-    #     try:
-    #         logger.info('Connecting to the PostgreSQL database...') # connect to the PostgreSQL server
-    #         conn = psycopg2.connect(**params)
-    #         cur = conn.cursor()                                     # create cursor
-    #         logger.info('PostgreSQL database version:')
-    #         cur.execute('SELECT version()')                         # execute a statement
-    #         db_version = cur.fetchone()                             # display the PostgreSQL database server version
-    #         logger.info(db_version)
-    #         cur.close()                                             # close the communication with the PostgreSQL
-    #     except (Exception, psycopg2.DatabaseError) as error:
-    #         logger.info(error)
-    #     finally:
-    #         if conn is not None:
-    #             conn.close()
-    #             logger.info('Database connection closed.')
-
-
-'''
-source table:
-    TRANSACTIONID          int64    f
-    FLIGHTDATE             int64    f
-    AIRLINECODE           object    f
-    AIRLINENAME           object    d
-    TAILNUM               object    f
-    FLIGHTNUM              int64    f - could become a dimension (these codes are reused)
-    ORIGINAIRPORTCODE     object    f
-    ORIGAIRPORTNAME       object    d
-    ORIGINCITYNAME        object    d
-    ORIGINSTATE           object    d
-    ORIGINSTATENAME       object    d
-    DESTAIRPORTCODE       object    f
-    DESTAIRPORTNAME       object    d
-    DESTCITYNAME          object    d
-    DESTSTATE             object    d
-    DESTSTATENAME         object    d
-    CRSDEPTIME             int64    f
-    DEPTIME              float64    f
-    DEPDELAY             float64    f
-    TAXIOUT              float64    f
-    WHEELSOFF            float64    f
-    WHEELSON             float64    f
-    TAXIIN               float64    f
-    CRSARRTIME             int64    f
-    ARRTIME              float64    f
-    ARRDELAY             float64    f
-    CRSELAPSEDTIME       float64    f
-    ACTUALELAPSEDTIME    float64    f
-    CANCELLED             object    f
-    DIVERTED              object    f
-    DISTANCE              object    f - could become a dimension
-
-fact table flights
-    id_trans
-    date_flight
-    id_airline
-    id_tailnum
-    id_flightnum
-    id_airport_orig
-    id_airport_dest
-    time_depart_crs
-    time_depart
-    time_depart_delay
-    time_taxi_out
-    time_wheelsoff
-    time_wheelson
-    time_taxi_in
-    time_arrive_crs
-    time_arrive
-    time_arrive_delay
-    time_elapsed_crs
-    time_elapsed_act
-    stat_cancelled
-    stat_diverted
-    stat_distance
-
-dim table airlines
-    id_airline
-    name
-
-dim table airports
-    id_airport
-    name
-    city
-    st
-    state (iso standard, useful?)
-
-dim table airplanes (hmmm - history... given incidents)
-    id_tailnum
-    id_airline (could be transferred)
-    date_service_first
-    date_service_last
-    cum_miles (calc from all segments)
-
-
-'''
-# get airlines: group by 'AIRLINENAME',
-# parse on ':' to strip name from code,
-# parse on '()' to strip note
-# get airports: group by 'ORIGINAIRPORTCODE' AND 'DESTAIRPORTCODE' (need both for full coverage),
-# then lift
-
-# -- problem definition/setup --
-'''
-Case Requirements
-1. Load the provided data into the PostgreSQL instance using the provided credentials.
-2. Create and load one Fact table to contain data about the flights.
-3. Create and load appropriate Dimension table(s).
-4. Create a view that joins your Fact table to your Dimension tables and returns columns useful for analysis.
-5. Prepare a 10-15 minute presentation to show your work, discuss your approach and any data quality issues that were encountered and your resolution to these issues.
-Fact Table
-1. Create an additional column called distance_group that bins the distance values into groups in 100 mile increments. Example: 94 miles is 0-100 miles. 274 miles is 201-300 miles.
-2. Create an additional column that indicates if the departure delay in minutes (DEPDELAY) is greater than 15.
-3. Create an additional column that indicates if the flight arrival time (ARRTIME) is the next day after the departure time (DEPTIME).
-4. Choose appropriate data types and perform conversions to load the data from the source into these types.
-5. Fix obviously bad data when encountered, if possible. Note these instances.
-Dimension Table(s)
-1. Create at least one dimension table and load it from the source data.
-2. Use your judgment about what columns from the source data should end up in the dimension tables. Be prepared to explain your decisions.
-3. Clean up the Airline Name column by removing the Airline Code from it.
-4. Clean up the Airport Name fields by removing the concatenated city and state.
-5. Fix obviously bad data when encountered, if possible. Note these instances.
-
-Postgres: iw-recruiting-test.cygkjm9anrym.us-west-2.rds.amazonaws.com
-Database: tests_data_engineering
-Username: candidate7206
-Password: vhWPmzt5a60cOZY5
-Schema: candidate7206
-(port n/a, defaults to 5432 if not provided)
-'''
 
 if __name__== '__main__':
     setDF(r=30)
-    flights = getFlights()
-    airlines = getAirlines(flights)
-    airports = getAirports(flights)
+    df1     = pd.DataFrame(readJSONFile('transactions-1.json')['transactions'])
+    df2     = pd.DataFrame(readJSONFile('transactions-2.json')['transactions'])
+    # flights = getFlights()
+    # airlines = getAirlines(flights)
+    # airports = getAirports(flights)
     # airplanes = getAirplanes(flights)
